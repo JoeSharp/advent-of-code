679e955956d9c5d4ec69495133cebfa7eb35ef9e
Joe Sharp <ratracejoe@hotmail.com>
2022-12-21 22:10:06 +0000

    day 4

---
+++ src/day4/index.ts
@@ -0,0 +1,72 @@
+import { processFile } from "../common/processFile";
+import simpleLogger from "../common/simpleLogger";
+import { AdventFunction } from "../common/types";
+
+export interface Range {
+  from: number;
+  to: number;
+}
+
+export const parseRange = (rangeStr: string): Range => {
+  const parts = rangeStr.split("-");
+
+  if (parts.length !== 2) {
+    throw new Error(`Invalid range string ${parts}`);
+  }
+
+  const from = parseInt(parts[0], 10);
+  const to = parseInt(parts[1], 10);
+
+  if (from > to) throw new Error(`Range is incorrect order ${from} -> ${to}`);
+
+  return { from, to };
+};
+
+export const parseRanges = (rangesStr: string): Range[] =>
+  rangesStr.split(",").map(parseRange);
+
+export const rangeIsContainedBy = (outer: Range, inner: Range): boolean => {
+  return outer.from <= inner.from && outer.to >= inner.to;
+};
+
+export const oneRangeContainsAnother = (
+  rangeA: Range,
+  rangeB: Range
+): boolean =>
+  rangeIsContainedBy(rangeA, rangeB) || rangeIsContainedBy(rangeB, rangeA);
+
+export const anyRangeOverlap = (rangeA: Range, rangeB: Range): boolean => {
+  if (rangeA.to < rangeB.from) return false;
+  if (rangeA.from > rangeB.to) return false;
+  return true;
+};
+
+const day4: AdventFunction = () =>
+  new Promise((resolve) => {
+    let numberThatHaveContainment = 0;
+    let numberThatHaveOverlap = 0;
+    processFile(
+      "./src/day4/input.txt",
+      (line) => {
+        const ranges = parseRanges(line);
+
+        if (ranges.length !== 2) {
+          simpleLogger.warn("A line contained unexpected number of ranges");
+          return;
+        }
+
+        if (oneRangeContainsAnother(ranges[0], ranges[1])) {
+          numberThatHaveContainment++;
+        }
+
+        if (anyRangeOverlap(ranges[0], ranges[1])) {
+          numberThatHaveOverlap++;
+        }
+      },
+      () => {
+        resolve([numberThatHaveContainment, numberThatHaveOverlap]);
+      }
+    );
+  });
+
+export default day4;
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,16 @@ import { AdventFunction } from "./common/types";
 import day1 from "./day1";
 import day2 from "./day2";
 import day3 from "./day3";
+import day4 from "./day4";
 
 const ADVENT_FUNCTIONS: Map<number, AdventFunction> = new Map();
 
 ADVENT_FUNCTIONS.set(1, day1);
 ADVENT_FUNCTIONS.set(2, day2);
 ADVENT_FUNCTIONS.set(3, day3);
+ADVENT_FUNCTIONS.set(4, day4);
 
-const DAY_TO_RUN = 3;
+const DAY_TO_RUN = 4;
 
 const fn = ADVENT_FUNCTIONS.get(DAY_TO_RUN);
 
