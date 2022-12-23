c845b5b828ea9839b963cce4189e2ab92d1bd307
Joe Sharp <ratracejoe@hotmail.com>
2022-12-23 15:17:35 +0000

    preparing for day 8 step 2

--- src/day8/index.test.ts
+++ src/day8/index.test.ts
@@ -1,11 +1,19 @@
-import day8, { readGridOfNumbers } from "./index";
+import day8, {
+  countVisibleTrees,
+  isTreeVisible,
+  isTreeVisibleFromBottom,
+  isTreeVisibleFromLeft,
+  isTreeVisibleFromRight,
+  isTreeVisibleFromTop,
+  readGridOfNumbers,
+} from "./index";
 
 describe("day8", () => {
   describe("Day 8", () => {
     it("handles demo input for part 1 correctly", async () => {
       const [part1] = await day8("./src/day8/testInput.txt");
 
-      expect(part1).toBe(1);
+      expect(part1).toBe(21);
     });
 
     it("handles demo input for part 2 correctly", async () => {
@@ -15,17 +23,70 @@ describe("day8", () => {
     });
   });
 
+  describe("countVisibleTrees", () => {
+    it("counts visible trees in test input", async () => {
+      const grid = await readGridOfNumbers("./src/day8/testInput.txt");
+
+      const result = countVisibleTrees(grid);
+
+      expect(result).toBe(21);
+    });
+  });
+
   describe("readGridOfNumbers", () => {
     it("handles test input", async () => {
       const grid = await readGridOfNumbers("./src/day8/testInput.txt");
 
-      expect(grid).toStrictEqual([
-        [3, 0, 3, 7, 3],
-        [2, 5, 5, 1, 2],
-        [6, 5, 3, 3, 2],
-        [3, 3, 5, 4, 9],
-        [3, 5, 3, 9, 0],
-      ]);
+      expect(grid).toStrictEqual({
+        rows: 5,
+        columns: 5,
+        content: [
+          [3, 0, 3, 7, 3],
+          [2, 5, 5, 1, 2],
+          [6, 5, 3, 3, 2],
+          [3, 3, 5, 4, 9],
+          [3, 5, 3, 9, 0],
+        ],
+      });
     });
+  });
+
+  describe("isTreeVisible", () => {
+    it.each`
+      row  | column | visible  | visLeft  | visRight | visTop   | visBottom
+      ${0} | ${0}   | ${true}  | ${true}  | ${false} | ${true}  | ${false}
+      ${0} | ${1}   | ${true}  | ${false} | ${false} | ${true}  | ${false}
+      ${1} | ${0}   | ${true}  | ${true}  | ${false} | ${false} | ${false}
+      ${1} | ${1}   | ${true}  | ${true}  | ${false} | ${true}  | ${false}
+      ${1} | ${2}   | ${true}  | ${false} | ${true}  | ${true}  | ${false}
+      ${1} | ${3}   | ${false} | ${false} | ${false} | ${false} | ${false}
+      ${2} | ${1}   | ${true}  | ${false} | ${true}  | ${false} | ${false}
+      ${2} | ${2}   | ${false} | ${false} | ${false} | ${false} | ${false}
+      ${2} | ${3}   | ${true}  | ${false} | ${true}  | ${false} | ${false}
+      ${3} | ${1}   | ${false} | ${false} | ${false} | ${false} | ${false}
+      ${3} | ${2}   | ${true}  | ${true}  | ${false} | ${false} | ${true}
+      ${3} | ${3}   | ${false} | ${false} | ${false} | ${false} | ${false}
+      ${4} | ${0}   | ${true}  | ${true}  | ${false} | ${false} | ${true}
+      ${4} | ${4}   | ${true}  | ${false} | ${true}  | ${false} | ${true}
+    `(
+      "$row, $column - Visible $visible, LRTB ($visLeft, $visRight, $visTop, $visBottom) ",
+      async ({
+        row,
+        column,
+        visible,
+        visRight,
+        visLeft,
+        visTop,
+        visBottom,
+      }) => {
+        const grid = await readGridOfNumbers("./src/day8/testInput.txt");
+
+        expect(isTreeVisibleFromLeft(grid, row, column)).toBe(visLeft);
+        expect(isTreeVisibleFromRight(grid, row, column)).toBe(visRight);
+        expect(isTreeVisibleFromTop(grid, row, column)).toBe(visTop);
+        expect(isTreeVisibleFromBottom(grid, row, column)).toBe(visBottom);
+        expect(isTreeVisible(grid, row, column)).toBe(visible);
+      }
+    );
   });
 });
--- src/day8/index.ts
+++ src/day8/index.ts
@@ -2,24 +2,128 @@ import * as fs from "fs";
 import readline from "readline";
 import { AdventFunction } from "../common/types";
 
-export const readGridOfNumbers = (filename: string): Promise<number[][]> => {
-  const grid: number[][] = [];
+export type TreeGrid = {
+  rows: number;
+  columns: number;
+  content: number[][];
+};
+
+export const readGridOfNumbers = (filename: string): Promise<TreeGrid> => {
+  const grid: TreeGrid = {
+    rows: 0,
+    columns: 0,
+    content: [],
+  };
   return new Promise((resolve) => {
     var r = readline.createInterface({
       input: fs.createReadStream(filename),
     });
-    r.on("line", (line) =>
-      grid.push(line.split("").map((i) => parseInt(i, 10)))
-    ).on("close", () => {
+    r.on("line", (line) => {
+      let row = line.split("").map((i) => parseInt(i, 10));
+      if (grid.columns === 0) {
+        grid.columns = row.length;
+      }
+      grid.content.push(row);
+      grid.rows++;
+    }).on("close", () => {
       resolve(grid);
     });
   });
 };
+type TreeGenerator = (
+  grid: TreeGrid,
+  row: number,
+  column: number
+) => Generator<number>;
 
-const day8: AdventFunction = (filename = "./src/day8/input.txt") => {
-  return new Promise((resolve) => {
-    resolve([1, 1]);
-  });
+export function* goLeft(
+  grid: TreeGrid,
+  row: number,
+  column: number
+): Generator<number> {
+  for (let c = column - 1; c >= 0; c--) {
+    yield grid.content[row][c];
+  }
+}
+export function* goRight(
+  grid: TreeGrid,
+  row: number,
+  column: number
+): Generator<number> {
+  for (let c = grid.columns - 1; c > column; c--) {
+    yield grid.content[row][c];
+  }
+}
+
+export function* goUp(
+  grid: TreeGrid,
+  row: number,
+  column: number
+): Generator<number> {
+  for (let r = row - 1; r >= 0; r--) {
+    yield grid.content[r][column];
+  }
+}
+
+export function* goDown(
+  grid: TreeGrid,
+  row: number,
+  column: number
+): Generator<number> {
+  for (let r = grid.rows - 1; r > row; r--) {
+    yield grid.content[r][column];
+  }
+}
+
+export const findBlockingTree =
+  (
+    generator: TreeGenerator
+  ): ((grid: TreeGrid, row: number, column: number) => boolean) =>
+  (grid, row, column) =>
+    ![...generator(grid, row, column)].find(
+      (otherTree) => otherTree >= grid.content[row][column]
+    );
+
+export const isTreeVisibleFromLeft = findBlockingTree(goLeft);
+export const isTreeVisibleFromRight = findBlockingTree(goRight);
+export const isTreeVisibleFromTop = findBlockingTree(goUp);
+export const isTreeVisibleFromBottom = findBlockingTree(goDown);
+
+export const isTreeVisible = (
+  grid: TreeGrid,
+  row: number,
+  column: number
+): boolean => {
+  return (
+    isTreeVisibleFromLeft(grid, row, column) ||
+    isTreeVisibleFromRight(grid, row, column) ||
+    isTreeVisibleFromTop(grid, row, column) ||
+    isTreeVisibleFromBottom(grid, row, column)
+  );
+};
+
+export const countVisibleTrees = (grid: TreeGrid): number => {
+  let visibleTrees = 0;
+
+  for (let row = 0; row < grid.rows; row++) {
+    for (let column = 0; column < grid.columns; column++) {
+      if (isTreeVisible(grid, row, column)) {
+        visibleTrees++;
+      }
+    }
+  }
+
+  return visibleTrees;
+};
+
+// How many visible from outside the grid?
+
+const day8: AdventFunction = async (filename = "./src/day8/input.txt") => {
+  const grid = await readGridOfNumbers(filename);
+
+  const partOne = countVisibleTrees(grid);
+
+  return [partOne, 1];
 };
 
 export default day8;
