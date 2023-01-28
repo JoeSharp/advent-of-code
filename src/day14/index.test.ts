05a12f4cab083999726b21c22e8f3d18e9a13fe5
Joe Sharp <ratracejoe@hotmail.com>
2023-01-28 16:25:37 +0000

    day14 part one done

--- src/day14/index.test.ts
+++ src/day14/index.test.ts
@@ -1,17 +1,246 @@
-import day14 from "./index";
+import { Point2D } from "../common/geometry";
+import day14, {
+  createRockMap,
+  dropSand,
+  dropSandUntilFallThrough,
+  findPointsOnPath,
+  getNextPoint,
+  isOccupied,
+  loadRockMap,
+  parseRockPath,
+  parseRockPoint,
+  RockMap,
+  rockPointToString,
+  SAND_STARTING_POINT,
+} from "./index";
+
+const TEST_INPUT_FILE = "./src/day14/testInput.txt";
 
 describe("day14", () => {
   describe("day14", () => {
     it("handles demo input for part 1 correctly", async () => {
-      const [part1] = await day14("./src/day14/testInput.txt");
+      const [part1] = await day14(TEST_INPUT_FILE);
 
-      expect(part1).toBe(1);
+      expect(part1).toBe(24);
     });
 
     it("handles demo input for part 2 correctly", async () => {
-      const [, part2] = await day14("./src/day14/testInput.txt");
+      const [, part2] = await day14(TEST_INPUT_FILE);
 
       expect(part2).toBe(1);
+    });
+  });
+
+  describe("dropSandUntilFallThrough", () => {
+    it("drops correct number of sand until full", async () => {
+      const rockMap = await loadRockMap(TEST_INPUT_FILE);
+
+      const result = dropSandUntilFallThrough(rockMap, SAND_STARTING_POINT);
+
+      expect(result).toBe(24);
+    });
+  });
+
+  describe("dropSand", () => {
+    it("drops sand and settles correctly", async () => {
+      const rockMap = await loadRockMap(TEST_INPUT_FILE);
+
+      expect(isOccupied(rockMap, { x: 500, y: 8 })).toBeFalsy();
+      const result = dropSand(rockMap, SAND_STARTING_POINT);
+
+      expect(result).toBeTruthy();
+      expect(isOccupied(rockMap, { x: 500, y: 8 })).toBeTruthy();
+    });
+
+    it("allows sand to fall through correctly", () => {});
+  });
+
+  describe("parseRockPoint", () => {
+    it("parses a valid point correctly", () => {
+      const result = parseRockPoint("5,6");
+      expect(result).toStrictEqual({
+        x: 5,
+        y: 6,
+      });
+    });
+    it("throws error for wrong number of parts", () => {
+      expect(() => parseRockPoint("5,6, 7")).toThrowError();
+    });
+    it("throws error for invalid numbers", () => {
+      expect(() => parseRockPoint("5,ba")).toThrowError();
+    });
+    it("throws error for nonsense", () => {
+      expect(() => parseRockPoint("foobar")).toThrowError();
+    });
+  });
+
+  describe("parseRockPath", () => {
+    it("parses a valid path correcty", () => {
+      const result = parseRockPath("503,4 -> 502,4 -> 502,9 -> 494,9");
+
+      expect(result).toStrictEqual([
+        { x: 503, y: 4 },
+        { x: 502, y: 4 },
+        { x: 502, y: 9 },
+        { x: 494, y: 9 },
+      ]);
+    });
+
+    it("throws error for wrong number of parts", () => {
+      expect(() =>
+        parseRockPoint("503,4,8 -> 502,4,7 -> 502,9 -> 494,9")
+      ).toThrowError();
+    });
+    it("throws error for invalid numbers", () => {
+      expect(() =>
+        parseRockPoint("503,ba -> 502,4 -> 502,cd -> 494,9")
+      ).toThrowError();
+    });
+    it("throws error for nonsense", () => {
+      expect(() => parseRockPoint("foobar")).toThrowError();
+    });
+  });
+
+  describe("getNextPoint", () => {
+    it("Locates the next space when there is space directly below", () => {
+      const rockMap: RockMap = {
+        contents: new Set<string>(),
+        lowestPoint: 0,
+      };
+
+      [
+        { x: 3, y: 4 },
+        { x: 4, y: 4 },
+        { x: 4, y: 3 },
+        { x: 5, y: 4 },
+      ]
+        .map(rockPointToString)
+        .forEach((p) => rockMap.contents.add(p));
+
+      const result = getNextPoint(rockMap, { x: 4, y: 1 });
+
+      expect(result).toStrictEqual({ x: 4, y: 2 });
+    });
+    it("Locates the next space when there is space to the left", () => {
+      const rockMap: RockMap = {
+        contents: new Set<string>(),
+        lowestPoint: 0,
+      };
+
+      [
+        { x: 3, y: 4 },
+        { x: 4, y: 4 },
+        { x: 4, y: 3 },
+        { x: 5, y: 4 },
+      ]
+        .map(rockPointToString)
+        .forEach((p) => rockMap.contents.add(p));
+
+      const result = getNextPoint(rockMap, { x: 4, y: 2 });
+
+      expect(result).toStrictEqual({ x: 3, y: 3 });
+    });
+    it("Locates the next space when there is space to the right", () => {
+      const rockMap: RockMap = {
+        contents: new Set<string>(),
+        lowestPoint: 0,
+      };
+
+      [
+        { x: 3, y: 3 },
+        { x: 3, y: 4 },
+        { x: 4, y: 4 },
+        { x: 4, y: 3 },
+        { x: 5, y: 4 },
+      ]
+        .map(rockPointToString)
+        .forEach((p) => rockMap.contents.add(p));
+
+      const result = getNextPoint(rockMap, { x: 4, y: 2 });
+
+      expect(result).toStrictEqual({ x: 5, y: 3 });
+    });
+  });
+
+  describe("findPointsOnPath", () => {
+    it("Works for horizontal line going left", () => {
+      const result = findPointsOnPath({ x: 5, y: 10 }, { x: 3, y: 10 });
+
+      expect(result.length).toBe(3);
+      expect(result[0]).toStrictEqual({ x: 3, y: 10 });
+      expect(result[1]).toStrictEqual({ x: 4, y: 10 });
+      expect(result[2]).toStrictEqual({ x: 5, y: 10 });
+    });
+    it("Works for horizontal line going right", () => {
+      const result = findPointsOnPath({ x: 3, y: 10 }, { x: 5, y: 10 });
+
+      expect(result.length).toBe(3);
+      expect(result[0]).toStrictEqual({ x: 3, y: 10 });
+      expect(result[1]).toStrictEqual({ x: 4, y: 10 });
+      expect(result[2]).toStrictEqual({ x: 5, y: 10 });
+    });
+    it("Works for vertical line going down", () => {
+      const result = findPointsOnPath({ x: 5, y: 10 }, { x: 5, y: 14 });
+
+      expect(result.length).toBe(5);
+      expect(result[0]).toStrictEqual({ x: 5, y: 10 });
+      expect(result[1]).toStrictEqual({ x: 5, y: 11 });
+      expect(result[2]).toStrictEqual({ x: 5, y: 12 });
+      expect(result[3]).toStrictEqual({ x: 5, y: 13 });
+      expect(result[4]).toStrictEqual({ x: 5, y: 14 });
+    });
+    it("Works for vertical line going up", () => {
+      const result = findPointsOnPath({ x: 5, y: 14 }, { x: 5, y: 10 });
+
+      expect(result.length).toBe(5);
+      expect(result[0]).toStrictEqual({ x: 5, y: 10 });
+      expect(result[1]).toStrictEqual({ x: 5, y: 11 });
+      expect(result[2]).toStrictEqual({ x: 5, y: 12 });
+      expect(result[3]).toStrictEqual({ x: 5, y: 13 });
+      expect(result[4]).toStrictEqual({ x: 5, y: 14 });
+    });
+  });
+
+  describe("createRockMap", () => {
+    it("works on the test input", () => {
+      const result = createRockMap([
+        [
+          { x: 498, y: 4 },
+          { x: 498, y: 6 },
+          { x: 496, y: 6 },
+        ],
+        [
+          { x: 503, y: 4 },
+          { x: 502, y: 4 },
+          { x: 502, y: 9 },
+          { x: 494, y: 9 },
+        ],
+      ]);
+
+      expect(result.lowestPoint).toBe(9);
+      expect(isOccupied(result, { x: 498, y: 4 })).toBeTruthy();
+      expect(isOccupied(result, { x: 498, y: 5 })).toBeTruthy();
+      expect(isOccupied(result, { x: 498, y: 6 })).toBeTruthy();
+      expect(isOccupied(result, { x: 497, y: 6 })).toBeTruthy();
+      expect(isOccupied(result, { x: 496, y: 6 })).toBeTruthy();
+      expect(isOccupied(result, { x: 496, y: 3 })).toBeFalsy();
+
+      expect(isOccupied(result, { x: 503, y: 4 })).toBeTruthy();
+      expect(isOccupied(result, { x: 502, y: 4 })).toBeTruthy();
+      expect(isOccupied(result, { x: 502, y: 5 })).toBeTruthy();
+      expect(isOccupied(result, { x: 502, y: 6 })).toBeTruthy();
+      expect(isOccupied(result, { x: 502, y: 7 })).toBeTruthy();
+      expect(isOccupied(result, { x: 502, y: 8 })).toBeTruthy();
+      expect(isOccupied(result, { x: 502, y: 9 })).toBeTruthy();
+      expect(isOccupied(result, { x: 501, y: 9 })).toBeTruthy();
+      expect(isOccupied(result, { x: 500, y: 9 })).toBeTruthy();
+      expect(isOccupied(result, { x: 499, y: 9 })).toBeTruthy();
+      expect(isOccupied(result, { x: 498, y: 9 })).toBeTruthy();
+      expect(isOccupied(result, { x: 497, y: 9 })).toBeTruthy();
+      expect(isOccupied(result, { x: 496, y: 9 })).toBeTruthy();
+      expect(isOccupied(result, { x: 495, y: 9 })).toBeTruthy();
+      expect(isOccupied(result, { x: 494, y: 9 })).toBeTruthy();
+      expect(isOccupied(result, { x: 493, y: 9 })).toBeFalsy();
     });
   });
 });
--- src/day14/index.ts
+++ src/day14/index.ts
@@ -1,9 +1,220 @@
-import { AdventFunction } from "../common/types";
+import simpleLogger from "../common/simpleLogger";
 
-const day14: AdventFunction = (filename = "./src/day14/input.txt") => {
-  return new Promise((resolve) => {
-    resolve([1, 1]);
+import * as fs from "fs";
+import readline from "readline";
+import { AdventFunction } from "../common/types";
+import { Point2D } from "../common/geometry";
+
+export const SAND_SOURCE: Point2D = {
+  x: 500,
+  y: 0,
+};
+
+export type RockPath = Point2D[];
+
+export const rockPointToString = ({ x, y }: Point2D) => `${x},${y}`;
+
+/**
+ * Given a series of points, we need to figure out which coordinates are occupied with rock.
+ * We will use a Set of string representations for all the occupied ones.
+ */
+export type RockMap = {
+  contents: Set<string>;
+  lowestPoint: number;
+};
+
+/**
+ * Identify all the points between two points.
+ * Including the endpoints.
+ *
+ * @param pointA The first point
+ * @param pointB The second point
+ */
+export function findPointsOnPath(pointA: Point2D, pointB: Point2D): Point2D[] {
+  let minX = Math.min(pointA.x, pointB.x);
+  let maxX = Math.max(pointA.x, pointB.x);
+  let minY = Math.min(pointA.y, pointB.y);
+  let maxY = Math.max(pointA.y, pointB.y);
+
+  const result: Point2D[] = [];
+  for (let x = minX; x <= maxX; x++) {
+    for (let y = minY; y <= maxY; y++) {
+      result.push({ x, y });
+    }
+  }
+
+  return result;
+}
+
+/**
+ * Given a set of paths, fills the gaps between them to figure out all the spaces that are occupied
+ *
+ * @param paths The paths to sketch out
+ * @returns A map of all the occupied rock spaces
+ */
+export function createRockMap(paths: RockPath[]): RockMap {
+  const contents = new Set<string>();
+  let lowestPoint = 0;
+
+  paths.forEach((path) => {
+    for (let i = 1; i < path.length; i++) {
+      findPointsOnPath(path[i - 1], path[i])
+        .map((p) => {
+          lowestPoint = Math.max(p.y, lowestPoint);
+          return rockPointToString(p);
+        })
+        .forEach((p) => contents.add(p));
+    }
   });
+
+  return { contents, lowestPoint };
+}
+
+/**
+ * Decide if a space is occupied by rock or sand
+ *
+ * @param rockMap The rock map
+ * @param point The point under evaluation
+ * @returns True if that point is occupied by rock
+ */
+export function isOccupied({ contents }: RockMap, point: Point2D): boolean {
+  return contents.has(rockPointToString(point));
+}
+
+/**
+ * Populate a spot in a rock map so it's now considered filled.
+ * @param rockMap The rock map to fill in
+ * @param point The point to occupy.
+ */
+export function occupyRockPoint({ contents }: RockMap, point: Point2D) {
+  contents.add(rockPointToString(point));
+}
+
+/**
+ * Locate the next point that a bit of sand will fall into.
+ *
+ * @param rockMap The current state of the rock
+ * @param point The point the sand is currently at
+ * @returns The next point for the sand to fall into
+ */
+export function getNextPoint(
+  rockMap: RockMap,
+  point: Point2D
+): Point2D | undefined {
+  // Go Down, Go Down-Left, Go Down-Right
+  // Find first of those spots that is not occupied
+  return [point.x, point.x - 1, point.x + 1]
+    .map((x) => ({ x, y: point.y + 1 }))
+    .find((p) => !isOccupied(rockMap, p));
+}
+
+/**
+ * Take a path part and parse it into a coordinate
+ * @param input The input string
+ * @returns The parsed coordinate
+ */
+export function parseRockPoint(input: string): Point2D {
+  const parts = input
+    .split(",")
+    .map((c) => parseInt(c, 10))
+    .filter((c) => !isNaN(c));
+
+  if (parts.length !== 2)
+    throw new Error("Invalid input, wrong number of parts when split by comma");
+
+  return {
+    x: parts[0],
+    y: parts[1],
+  };
+}
+
+/**
+ * Parse a line from the input file into a complete set of points defining the path.
+ * @param input The entire line from the input file
+ * @returns the parsed path
+ */
+export function parseRockPath(input: string): RockPath {
+  return input
+    .split("->")
+    .map((p) => p.trim())
+    .map(parseRockPoint);
+}
+
+/**
+ * Load the input filename and parse out the entire rock map.
+ * @param filename the file to process
+ * @returns
+ */
+export function loadRockMap(filename: string): Promise<RockMap> {
+  return new Promise<RockMap>((resolve) => {
+    const paths: RockPath[] = [];
+
+    var r = readline.createInterface({
+      input: fs.createReadStream(filename),
+    });
+    r.on("line", (line) => {
+      paths.push(parseRockPath(line));
+    }).on("close", () => {
+      resolve(createRockMap(paths));
+    });
+  });
+}
+
+/**
+ * Drops a blob of sand into the given rock structure from a set point.
+ * @param rockMap The rock map on which to operate. The map is mutated by this operation
+ * @param startingPoint The point at which the sand is being dropped
+ * @returns Boolean to indicate if the sand settled.
+ */
+export function dropSand(rockMap: RockMap, ingressPoint: Point2D): boolean {
+  let currentLocation = { ...ingressPoint };
+
+  while (currentLocation.y < rockMap.lowestPoint) {
+    const nextPoint = getNextPoint(rockMap, currentLocation);
+
+    // If there is no 'next point', the sand must have settled
+    if (nextPoint === undefined) {
+      occupyRockPoint(rockMap, currentLocation);
+      return true;
+    }
+
+    currentLocation = nextPoint;
+  }
+
+  return false;
+}
+
+/**
+ *
+ * @param rockMap The rock map on which to operate, it will be mutated by this operation
+ * @param ingressPoint
+ */
+export function dropSandUntilFallThrough(
+  rockMap: RockMap,
+  ingressPoint: Point2D
+): number {
+  let settled = false;
+  let numberOfGrains = 0;
+  do {
+    settled = dropSand(rockMap, ingressPoint);
+    if (settled) {
+      numberOfGrains++;
+    }
+  } while (settled);
+
+  return numberOfGrains;
+}
+
+export const SAND_STARTING_POINT: Point2D = {
+  x: 500,
+  y: 0,
+};
+
+const day14: AdventFunction = async (filename = "./src/day14/input.txt") => {
+  const rockMap = await loadRockMap(filename);
+  const partOne = dropSandUntilFallThrough(rockMap, SAND_STARTING_POINT);
+
+  return [partOne, 1];
 };
 
 export default day14;
