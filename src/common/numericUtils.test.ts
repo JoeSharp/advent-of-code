24be6e18fd77ae1f7fee10f08595c03a4fc58116
Joe Sharp <ratracejoe@hotmail.com>
2024-11-30 20:42:52 +0000

    just adding tests for numeric utils, tryign to remember typescript

--- src/common/numericUtils.test.ts
+++ src/common/numericUtils.test.ts
@@ -1,4 +1,8 @@
-import { reverseNumericSort } from "./numericUtils";
+import { 
+  numericSort, 
+  isDivisibleBy, 
+  reverseNumericSort 
+} from "./numericUtils";
 
 describe("numericUtils", () => {
   describe("reverseNumericSort", () => {
@@ -8,5 +12,22 @@ describe("numericUtils", () => {
 
       expect(items).toStrictEqual([90, 54, 8, 7, 3]);
     });
+  });
+  describe("NumericSort", () => {
+    it("sorts in numeric order", () => {
+      const items = [58, 23, 90, 7, 54];
+      items.sort(numericSort);
+
+      expect(items).toStrictEqual([7, 23, 54, 58, 90]);
+    });
+  });
+  describe("isDivisibleBy", () => {
+    it.each`
+      input | divisor | expected
+      ${8} | ${2}| ${true}
+      `('$input divisible by $divisor = $expected', ({input, divisor, expected}) => {
+        const result = isDivisibleBy(input, divisor);
+        expect(result).toBe(expected);
+      });
   });
 });
