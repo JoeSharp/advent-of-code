b682e99e31793f1be9fe22e78844447fec3bde29
Joe Sharp <ratracejoe@hotmail.com>
2024-12-03 06:05:34 +0000

    2024 day 3

--- src/2024/day3/index.test.ts
+++ src/2024/day3/index.test.ts
@@ -1,19 +1,20 @@
 import day3 from "./index";
 
-const TEST_INPUT_FILE = "./src/2024/day3/testInput.txt";
+const TEST_INPUT_FILE_1 = "./src/2024/day3/testInput1.txt";
+const TEST_INPUT_FILE_2 = "./src/2024/day3/testInput2.txt";
 
 describe("day3", () => {
   describe("day3", () => {
     it("handles demo input for part 1 correctly", async () => {
-      const [part1] = await day3(TEST_INPUT_FILE);
+      const [part1] = await day3(TEST_INPUT_FILE_1);
 
-      expect(part1).toBe(1);
+      expect(part1).toBe(161);
     });
 
     it("handles demo input for part 2 correctly", async () => {
-      const [, part2] = await day3(TEST_INPUT_FILE);
+      const [, part2] = await day3(TEST_INPUT_FILE_2);
 
-      expect(part2).toBe(1);
+      expect(part2).toBe(48);
     });
   });
 });
--- src/2024/day3/index.ts
+++ src/2024/day3/index.ts
@@ -1,7 +1,61 @@
 import { AdventFunction } from "../../common/types";
+import { loadEntireFile } from "../../common/processFile";
+
+export type Multiplication = [number, number];
+
+export function getMultiplications(line: string): Multiplication[] {
+  const regex = /mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)/g;
+  const matches = [...line.matchAll(regex)];
+
+  return matches
+    .filter((m) => !!m.groups)
+    .map((match) => {
+      const { first, second } = match.groups!;
+      return [parseInt(first), parseInt(second)];
+    });
+}
+
+export function getCancellableMultiplications(line: string): Multiplication[] {
+  const regex =
+    /(?<dont>don't\(\))|(?<do>do\(\))|mul\((?<first>\d{1,3}),(?<second>\d{1,3})\)/g;
+  const matches = [...line.matchAll(regex)];
+
+  let doMult = true;
+  const multiplications: Multiplication[] = [];
+
+  matches.forEach((match) => {
+    if (match.groups == undefined) return;
+
+    if (match.groups.do) {
+      doMult = true;
+      return;
+    }
+    if (match.groups.dont) {
+      doMult = false;
+      return;
+    }
+    if (doMult) {
+      const { first, second } = match.groups;
+      multiplications.push([parseInt(first), parseInt(second)]);
+    }
+  });
+  return multiplications;
+}
 
 const day3: AdventFunction = async (filename = "./src/2024/day3/input.txt") => {
-  return [1, 1];
+  const contents = await loadEntireFile(filename);
+
+  const line = contents.join('');
+
+  const part1 = getMultiplications(line)
+    .map(([a, b]) => a * b)
+    .reduce((acc, curr) => acc + curr, 0);
+
+  const part2 = getCancellableMultiplications(line)
+    .map(([a, b]) => a * b)
+    .reduce((acc, curr) => acc + curr, 0);
+
+  return [part1, part2];
 };
 
 export default day3;
