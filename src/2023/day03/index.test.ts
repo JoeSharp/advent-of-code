cd34da37d4c7c09545ad0d677e96cca3afa9b924
Joe Sharp <ratracejoe@hotmail.com>
2024-12-11 22:40:20 +0000

    starting day3 of 2023

--- src/2023/day03/index.test.ts
+++ src/2023/day03/index.test.ts
@@ -6,7 +6,7 @@ describe("day03", () => {
   it("handles demo input for part 1 correctly", async () => {
     const [part1] = await day03(TEST_INPUT_FILE);
 
-    expect(part1).toBe(1);
+    expect(part1).toBe(4361);
   });
 
   it("handles demo input for part 2 correctly", async () => {
--- src/2023/day03/index.ts
+++ src/2023/day03/index.ts
@@ -1,6 +1,10 @@
 import { AdventFunction } from "../../common/types";
+import { loadEntireFileAsGrid } from '../../common/processFile';
+
 
 const day03: AdventFunction = async (filename = "./src/2023/day03/input.txt") => {
+  const lines: string[][] = await loadEntireFileAsGrid(filename);
+
   return [1, 1];
 };
 
