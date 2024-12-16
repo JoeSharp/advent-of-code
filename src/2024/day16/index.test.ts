cf65b10acfebf444f4a343f2b9aecd77d73cfd0a
Joe Sharp <ratracejoe@hotmail.com>
2024-12-16 08:05:05 +0000

    day 16, pulling in my dijkstra from comp-sci-maths-lib

--- src/2024/day16/index.test.ts
+++ src/2024/day16/index.test.ts
@@ -1,15 +1,25 @@
-import day16 from "./index";
+import day16, { loadRawMaze } from "./index";
 
 const TEST_INPUT_FILE = "./src/2024/day16/testInput.txt";
 
 describe("day16", () => {
-  it("handles demo input for part 1 correctly", async () => {
-    const [part1] = await day16(TEST_INPUT_FILE);
 
-    expect(part1).toBe(1);
+  it("loadRawMaze", async () => {
+    const rawMaze = await loadRawMaze(TEST_INPUT_FILE);
+
+    expect(rawMaze.start).toStrictEqual([13, 1]);
+    expect(rawMaze.end).toStrictEqual([1, 13]);
+    expect(rawMaze.contents.length).toBe(15);
+    expect(rawMaze.contents[0].length).toBe(15);
   });
 
-  it("handles demo input for part 2 correctly", async () => {
+  it.skip("handles demo input for part 1 correctly", async () => {
+    const [part1] = await day16(TEST_INPUT_FILE);
+
+    expect(part1).toBe(11048);
+  });
+
+  it.skip("handles demo input for part 2 correctly", async () => {
     const [, part2] = await day16(TEST_INPUT_FILE);
 
     expect(part2).toBe(1);
--- src/2024/day16/index.ts
+++ src/2024/day16/index.ts
@@ -1,4 +1,37 @@
 import { AdventFunction } from "../../common/types";
+import { Position, findInstancesOf, gridArrayToStr } from '../../common/arrayUtils';
+import { loadEntireFileAsGrid } from '../../common/processFile';
+
+export const START = 'S';
+export const END = 'E';
+export const WALL = '#';
+
+export const MOVE_SCORE = 1;
+export const ROTATE_SCORE = 1000;
+
+interface RawMaze {
+  contents: string[][];
+  start: Position;
+end: Position;
+}
+
+export async function loadRawMaze(filename: string): RawMaze {
+  const contents = await loadEntireFileAsGrid(filename);
+  
+  const sTiles = findInstancesOf(contents, v => v === START);
+  const eTiles = findInstancesOf(contents, v => v === END);
+
+  console.log(gridArrayToStr(contents));
+
+  if (sTiles.length !== 1) throw new Error('Multiple start points?');
+  if (eTiles.length !== 1) throw new Error('Multiple end points?');
+
+  return {
+    contents,
+    start: sTiles[0],
+    end: eTiles[0]
+  }
+}
 
 const day16: AdventFunction = async (
   filename = "./src/2024/day16/input.txt",
