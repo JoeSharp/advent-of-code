2a99a4727437badafbf8ad375927b62fe51fcb7d
Joe Sharp <ratracejoe@hotmail.com>
2024-12-19 10:37:05 +0000

    day 19, naive and computationally infeasible solution

--- src/2024/day19/index.test.ts
+++ src/2024/day19/index.test.ts
@@ -1,15 +1,55 @@
-import day19 from "./index";
+import day19, {
+  canMakePattern,
+  loadTowelProblem,
+  canMakeNextPartOfPattern,
+} from "./index";
 
 const TEST_INPUT_FILE = "./src/2024/day19/testInput.txt";
 
 describe("day19", () => {
+  it.each`
+    towel         | desired                                | index | expected
+    ${["f", "a"]} | ${["f", "a", "j", "o", "g"]}           | ${0}  | ${true}
+    ${["f", "a"]} | ${["f", "a", "j", "o", "g"]}           | ${1}  | ${false}
+    ${["j", "o"]} | ${["f", "a", "j", "o", "g"]}           | ${2}  | ${true}
+    ${["j", "b"]} | ${["f", "a", "j", "o", "g"]}           | ${2}  | ${false}
+    ${["j", "b"]} | ${["f", "a", "j", "o", "g", "j", "b"]} | ${2}  | ${false}
+    ${["j", "b"]} | ${["f", "a", "j", "o", "g", "j", "b"]} | ${5}  | ${true}
+  `(
+    "canMakeNextPartOfPattern $towel in $desired at $index = $expected",
+    ({ towel, desired, index, expected }) => {
+      const result = canMakeNextPartOfPattern(towel, desired, index);
+
+      expect(result).toBe(expected);
+    },
+  );
+
+  it.each`
+    towelSet                                          | desired                           | expected
+    ${["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]} | ${["b", "r", "w", "r", "r"]}      | ${true}
+    ${["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]} | ${["b", "g", "g", "r"]}           | ${true}
+    ${["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]} | ${["g", "b", "b", "r"]}           | ${true}
+    ${["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]} | ${["r", "r", "b", "g", "b", "r"]} | ${true}
+    ${["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]} | ${["u", "b", "w", "u"]}           | ${false}
+    ${["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]} | ${["b", "w", "u", "r", "r", "g"]} | ${true}
+    ${["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]} | ${["b", "r", "g", "r"]}           | ${true}
+    ${["r", "wr", "b", "g", "bwu", "rb", "gb", "br"]} | ${["b", "b", "r", "g", "w", "b"]} | ${false}
+  `(
+    "canMakePattern $desired with any of $towelSet = $expected",
+    ({ desired, towelSet, expected }) => {
+      const result = canMakePattern(towelSet, desired);
+
+      expect(result).toBe(expected);
+    },
+  );
+
   it("handles demo input for part 1 correctly", async () => {
     const [part1] = await day19(TEST_INPUT_FILE);
 
-    expect(part1).toBe(1);
+    expect(part1).toBe(6);
   });
 
-  it("handles demo input for part 2 correctly", async () => {
+  it.skip("handles demo input for part 2 correctly", async () => {
     const [, part2] = await day19(TEST_INPUT_FILE);
 
     expect(part2).toBe(1);
--- src/2024/day19/index.ts
+++ src/2024/day19/index.ts
@@ -1,9 +1,82 @@
 import { AdventFunction } from "../../common/types";
+import { loadEntireFile } from "../../common/processFile";
+
+export type Towel = string[];
+export type TowelSet = Towel[];
+
+export interface TowelProblem {
+  towelSet: TowelSet;
+
+  desired: Towel[];
+}
+
+function parseTowel(input: string): Towel {
+  return input.trim().split("");
+}
+
+function parseTowelSet(input: string): TowelSet {
+  return input
+    .split(",")
+    .map((d) => d.trim())
+    .map(parseTowel) as TowelSet;
+}
+
+async function loadTowelProblem(filename: string): Promise<TowelProblem> {
+  const contents = await loadEntireFile(filename);
+
+  return {
+    towelSet: parseTowelSet(contents[0]),
+    desired: contents.slice(2).map(parseTowel),
+  };
+}
+
+export function canMakeNextPartOfPattern(
+  towel: Towel,
+  desired: Towel,
+  index: number,
+): boolean {
+  if (towel.length + index > desired.length) return false;
+
+  for (let i = 0; i < towel.length; i++) {
+    if (desired[i + index] !== towel[i]) return false;
+  }
+
+  return true;
+}
+
+export function canMakePattern(
+  towelSet: TowelSet,
+  desired: Towel,
+  index: number = 0,
+): boolean {
+    console.log(`Looking to make ${desired.join(',')} from ${index}`);
+
+  return towelSet.findIndex((towel, ti) => {
+    if (canMakeNextPartOfPattern(towel, desired, index)) {
+      let nextIndex = index + towel.length;
+      if (nextIndex === desired.length) {
+        return true;
+      } else {
+        return canMakePattern(towelSet, desired, nextIndex);
+      }
+    } else {
+      return false;
+    }
+  }) !== -1;
+}
+
+async function part1(filename: string): Promise<number> {
+  const problem = await loadTowelProblem(filename);
+  return problem.desired.filter((d) => canMakePattern(problem.towelSet, d))
+    .length;
+}
 
 const day19: AdventFunction = async (
   filename = "./src/2024/day19/input.txt",
 ) => {
-  return [1, 1];
+  const p1 = await part1(filename);
+
+  return [p1, 1];
 };
 
 export default day19;
