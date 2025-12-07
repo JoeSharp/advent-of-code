1c0a22b9dae382e1406a84204ab04978d7acecd1
Joe Sharp <ratracejoe@hotmail.com>
2025-12-07 14:24:03 +0000

    2025 day 4 prep

--- src/2025/day04/index.test.ts
+++ src/2025/day04/index.test.ts
@@ -6,7 +6,7 @@ describe("day04", () => {
   it("handles demo input for part 1 correctly", async () => {
     const [part1] = await dayX(TEST_INPUT_FILE);
 
-    expect(part1).toBe(1);
+    expect(part1).toBe(13);
   });
 
   it("handles demo input for part 2 correctly", async () => {
